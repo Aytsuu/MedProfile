3ca10b0a98cd56201f9c8d5fc01a5fe3a06f5535
antonux <marcusremigoso@gmail.com>
2024-10-30 16:50:57 +0800

    HomeUI#1

--- MedProfile/src/Home.jsx
+++ MedProfile/src/Home.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 
 export default function Home() {
-  const Home = 
-      <div className="Category Box1">
-        <h1>
-          Testing
-        </h1>
-
+  const box_class = "bg-red-800 size-96"
+  const Home = (
+    <div className="flex gap-10 my-auto">
+      <div className={box_class}>
       </div>
+
+      <div className={box_class}>
+      </div>
+
+      <div className={box_class}>
+      </div>
+
+    </div>
+  );
   return (
     <>
-    {Home}
+      {Home}
     </>
   )
 }

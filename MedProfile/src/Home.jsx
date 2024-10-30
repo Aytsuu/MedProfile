011b6488a0d2ab0eea078dd27336bfd5963ce433
antonux <marcusremigoso@gmail.com>
2024-10-30 14:25:21 +0800

    Third commit

--- MedProfile/src/Home.jsx
+++ MedProfile/src/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 export default function Home() {
   const Home = 
-      <div className="Category">
+      <div className="Category Box">
+        <h1>
+          Test
+        </h1>
 
       </div>
   return (

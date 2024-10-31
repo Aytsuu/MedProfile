3440a59f0e0914ee66271fdb2488e78cb00c2b9b
Aytsuu <paoloaraneta008@gmail.com>
2024-10-31 16:38:15 +0800

    added comments and image

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,35 @@ import FPnewPass from './ForgotPassword-2.jsx'
 
 const App = () =>{
 
+  // Create a routes that link buttons to other files/pages
   const router = createBrowserRouter([
     {
+      // Default link to sign in page
       path: "/",
       element: <SignIn/>
     },
     {
+      // Link to sign in page
       path: "/signin",
       element: <SignIn/>
     },
     {
+      // Link to sign up page
       path: "/signup",
       element: <SignUp/>
     },
     {
+      // Link to forgot password -- enter email page
       path: "/forgotpass-1",
       element: <FPgetEmail/>
     },
     {
+      // Link to forgot password -- email verification page
       path: "/accountrecover",
       element: <FPgetCode type={"ACCOUNT RECOVERY"}/>
-    },{
+    },
+    {
+      // Link to forgot password -- set new password page
       path: "/forgotpass-2",
       element: <FPnewPass/>
     }

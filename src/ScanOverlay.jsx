c473669da371e035eb94db848e579d15c51cf3b4
Huesef <whiteropolisz@gmail.com>
2024-11-02 15:32:53 +0800

    ScanOverlay file created, Sidebar configured for Overlay, Uploaded Overlay resources

--- src/ScanOverlay.jsx
+++ src/ScanOverlay.jsx
@@ -1,5 +1,16 @@
+import ScanOverlayImg from './ResourceImage/ScanOverlay.png'
+import CancelButt from "./ResourceImage/cancel.png";
 
+function ScanOverlay({ onClose })  {
+    const FingerScan = (
+        <div className="flex items-center justify-center h-screen">
+            <div className='absolute inset-0 bg-black opacity-70'></div>
+            <img src={ScanOverlayImg} alt="" className="h-auto w-auto z-10"/>
+            <img src={CancelButt} className='absolute top-5 right-5 size-14' onClick={onClose}/>
+        </div>
+    )
 
-function ScanOverlay()  {
-    
+    return FingerScan
 }
+
+export default ScanOverlay

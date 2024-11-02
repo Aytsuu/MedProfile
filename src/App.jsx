9ab27cfc70b6a995a84933101fd404448c9234c6
Huesef <whiteropolisz@gmail.com>
2024-11-02 23:43:55 +0800

    Routing configured

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from './Sidebar'
 import Registration from './Registration'
 import Records from './Records'
+import LockedRegistration from './lockedRegistration'
 
 function App() {
   return(
@@ -13,7 +14,7 @@ function App() {
           <Route path="/register" element={<Registration/>} />
           <Route path="/records"  element={<Records/>} />
           {/* Tentative cause instead of Registration it should be the profile panel */}
-          <Route path="/profile" element={<Registration/>}/>
+          <Route path="/profile" element={<LockedRegistration/>}/>
         </Routes>
       </div>  
     </Router>
